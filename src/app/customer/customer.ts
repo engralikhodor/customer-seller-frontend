7e167b0f2f9328486c8d89f2df263abf617d2db3
alikhodor <ali@cityrelay.com>
2022-12-29 18:15:33 +0200

    export interface Customer

---
+++ src/app/customer/customer.ts
@@ -0,0 +1,14 @@
+export interface Customer {
+    id: number,
+    first_name: string,
+    last_name: string,
+    country: string,
+    city: string,
+    currency: string,
+    pay_per_hour: number,
+    gender: string,
+    email: string,
+    phone: number,
+    date_of_birth: string,
+    is_deleted: boolean
+}
