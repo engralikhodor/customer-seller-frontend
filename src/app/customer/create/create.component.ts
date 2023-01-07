c5d13e74e2351698061fb3bb6d21e7323a87eb16
alikhodor <ali@cityrelay.com>
2023-01-07 10:42:38 +0200

    * customer -> create

--- src/app/customer/create/create.component.ts
+++ src/app/customer/create/create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CustomerService } from '../customer.service';
 
 @Component({
   selector: 'app-create',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
-
+  public form: FormGroup = new FormGroup({});
+  /*------------------------------------------*/
+  constructor(
+    public customerservice: CustomerService,
+    private router: Router
+  ) { }
+  /*------------------------------------------*/
   ngOnInit(): void {
+    this.form = new FormGroup({
+      //pattern for English and Arabic 
+      first_name: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z\u0600-\u06FF ]*')]),
+      last_name: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z\u0600-\u06FF ]*')]),
+      email: new FormControl('', [Validators.email]),
+      phone: new FormControl('', [Validators.required, Validators.pattern("^[0-9]*$")]),
+      country: new FormControl('', [Validators.required]),
+      city: new FormControl('', [Validators.required]),
+      gender: new FormControl('', [Validators.required])
+    });
   }
+  /*------------------------------------------*/
+  get f() {
+    return this.form.controls;
+  }
+  /*------------------------------------------*/
+  submit() {
+    console.log(this.form.value);
+    this.customerservice.create(this.form.value).subscribe(
+      res => {
+        console.log("saved");
+        this.router.navigateByUrl("customer/index");
+      }
+    );
+  }
+  /*------------------------------------------*/
+
 
 }
